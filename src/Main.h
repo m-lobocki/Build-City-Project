3a0d32c25ec9a07b440b643af98f3654a817b2f7
m-lobocki <mlobocki@outlook.com>
2019-01-13 20:50:06 +0100

    Cleared unnecessary comments

--- src/Main.h
+++ src/Main.h
@@ -1,9 +1,4 @@
-﻿// Build-City-Project.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
+﻿#pragma once
 
 #include <iostream>
 #include <SFML/Window.hpp>
-
-// TODO: Reference additional headers your program requires here.
