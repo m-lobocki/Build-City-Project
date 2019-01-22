df8319c002be5396e196418f8db227d08e852452
m-lobocki <mlobocki@outlook.com>
2019-01-22 16:58:30 +0100

    Namespace to statically get loaded resources

---
+++ src/Resources.cpp
@@ -0,0 +1,22 @@
+#include "Resources.h"
+
+std::string font_name[] = { "OpenSans-Regular", "OpenSans-Italic", "OpenSans-Light", "OpenSans-Bold" };
+sf::Font fonts[4];
+
+bool Resources::load_fonts()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		fonts[i] = sf::Font();
+		if(!fonts[i].loadFromFile("fonts/" + font_name[i] + ".ttf"))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+sf::Font& Resources::get_font(FontWeight weight)
+{
+	return fonts[int(weight)];
+}
---
+++ src/Resources.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "SFML/Graphics/Font.hpp"
+
+enum class FontWeight
+{
+	Regular = 0,
+	Italic = 1,
+	Light = 2,
+	Bold = 3
+};
+
+namespace Resources
+{
+	bool load_fonts();
+	sf::Font& get_font(FontWeight weight);
+}
